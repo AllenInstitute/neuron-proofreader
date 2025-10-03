8e93f9df440eae1d2365edc75a971487ab95490b
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-10-03 12:20:07 -0700

    Feat clip merge fragments (#492)
    * feat: clip fragments to groundtruth
    
    * bug: upd component ids after relabel
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/neuron_proofreader/merge_proofreading/train_detection.py
+++ src/neuron_proofreader/merge_proofreading/train_detection.py
@@ -134,7 +134,7 @@ class MergeSiteDataset:
 
         # Build kd-tree from merge sites
         for brain_id in self.merge_sites_df["brain_id"].unique():
-            pts = extract_merge_xyz(self.merge_sites_df, brain_id)
+            pts = get_gt_merge_sites(self.merge_sites_df, brain_id)
             self.merge_site_kdtrees[brain_id] = KDTree(pts)
 
     def _init_kdtree(self, graphs):
@@ -350,13 +350,11 @@ class MergeSiteDataset:
     def clip_fragments_to_groundtruth(self, brain_id, graph):
         assert brain_id in self.gt_graphs, "Must load GT before fragments!"
         kdtree = KDTree(self.gt_graphs[brain_id].node_xyz)
-        cnt = 0
         for i, xyz in enumerate(graph.node_xyz):
             if i in graph:
                 d, _ = kdtree.query(xyz)
-                if d > 30:
+                if d > 80:
                     graph.remove_node(i)
-                    cnt += 1
 
     def __len__(self):
         """
@@ -503,9 +501,9 @@ def get_brain_segmentation_pairs(merge_sites_df, idxs):
     return brain_segmentation_pairs
 
 
-def extract_merge_xyz(merge_sites_df, brain_id):
+def get_gt_merge_sites(merge_sites_df, brain_id):
     """
-    Gets the xyz coordinates of merge sites for a given brain.
+    Gets the xyz coordinates of ground truth merge sites for a given brain.
 
     Parameters
     ----------
