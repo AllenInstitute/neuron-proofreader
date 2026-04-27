15b4eda6726c3236ab754e6eba173e3fe5e29bd1
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2026-04-27 11:32:56 -0700

    refactor: updated multistep (#692)
    * refactor: updated multistep
    
    * refactor: updated multistep
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/neuron_proofreader/split_proofreading/split_inference.py
+++ src/neuron_proofreader/split_proofreading/split_inference.py
@@ -146,7 +146,9 @@ class InferencePipeline:
         self.log(f"Module Runtime: {elapsed:.2f} {unit}\n")
 
     # --- Pipelines ---
-    def __call__(self, search_radius):
+    def __call__(
+        self, search_radius, dt=0.1, min_threshold=0.75, removal_threshold=0.3
+    ):
         """
         Executes the full inference pipeline.
 
@@ -154,44 +156,39 @@ class InferencePipeline:
         ----------
         search_radius : float
             Search radius (in microns) used to generate proposals.
+        dt : float, optional
+            Increment that acceptance threshold is lowered by. Default is 0.1.
+        min_threshold : float, optional
+            Minimum threshold for accepting proposals. Default is 0.75.
+        removal_threshold : float, optional
+            Proposals with model predictions less than this value are removed.
+            Default is 0.3.
         """
-        # Generate proposal
-        t0 = time()
-        self.generate_proposals(search_radius)
-        preds = self.predict_proposals()
-
-        # Update graph
-        self.merge_with_threshold_schedule(preds, self.config.ml.threshold)
-
-        # Report results
-        t, unit = util.time_writer(time() - t0)
-        self.log(self.dataset.summary(prefix="\nFinal"))
-        self.log(f"Total Runtime: {t:.2f} {unit}\n")
-        self.save_results()
-
-    def multistep(self, search_radius, low_threshold=0.3, dt=0.1):
-        t0 = time()
+        # Generate proposals
         self.generate_proposals(search_radius)
         total_proposals = self.dataset.n_proposals()
+
+        # Run inference
+        cnt = 0
+        t0 = time()
         for only_leaf2leaf in [True, False]:
-            cnt = 0
             name = "_leaf2leaf" if only_leaf2leaf else ""
             new_threshold = 0.99
             while self.dataset.proposals:
                 # Generate predictons
                 cnt += 1
-                print(f"Threshold={new_threshold} w/ only_leaf2leaf={only_leaf2leaf}")
-                preds = self.predict_proposals(suffix=f"{name}_round={cnt}")
+                self.log(f"Threshold={new_threshold} w/ only_leaf2leaf={only_leaf2leaf}")
+                preds = self.predict_proposals(suffix=f"{name}_round={cnt}_threshold={new_threshold}")
 
                 # Merge accetped proposals
                 cur_threshold = new_threshold
                 self.merge_with_threshold_schedule(
                     preds, cur_threshold, only_leaf2leaf=only_leaf2leaf
                 )
-                self.filter_proposals(preds, low_threshold)
+                self.filter_proposals(preds, removal_threshold)
 
                 # Update threshold
-                new_threshold = max(cur_threshold - dt, self.config.ml.threshold)
+                new_threshold = max(cur_threshold - dt, min_threshold)
                 if cur_threshold == new_threshold:
                     break
 
@@ -205,10 +202,18 @@ class InferencePipeline:
 
     # --- Core Routines ---
     def filter_proposals(self, preds, threshold):
+        # Remove based on model predictions and mergeability
         cnt = 0
         for proposal, pred in preds.items():
             is_valid = self.dataset.is_mergeable(*proposal)
             if pred < threshold or not is_valid:
+                self.dataset.remove_proposal(proposal)
+                cnt += 1
+
+        # Sanity check
+        for proposal in self.dataset.list_proposals():
+            i, j = proposal
+            if self.dataset.degree[i] > 2 or self.dataset.degree[j] > 2:
                 self.dataset.remove_proposal(proposal)
                 cnt += 1
 
