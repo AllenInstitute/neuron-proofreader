0d0e99ebd43b07f70bdc4deb0f71ead6dd4f6c60
Anna Grim <anna.grim@alleninstitute.org>
2026-07-15 04:40:53 +0000

    Edited app-panel.json, evaluate.py, Dockerfile, postInstall; and changes in 1 other files

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,194 +1,245 @@
-from segmentation_skeleton_metrics.datamodules.graph_loading import (
-    DataLoader,
-)
-from segmentation_skeleton_metrics.utils.img_util import TensorStoreImage
+"""
+Created on Wed June 3 12:00:00 2026
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+Code evaluating merge detection models.
+
+"""
+
 from scipy.spatial import KDTree
 from tqdm import tqdm
-from zipfile import ZipFile
 
-import ast
+import argsparse
 import numpy as np
 import os
-import pandas as pd
 
-from neuron_proofreader.utils import swc_util, util
-from neuron_proofreading_evaluation.proofread_splits import (
-    data_handling as data_util,
+from neuron_proofreader.configs import ImageConfig
+from neuron_proofreader.skeleton_graph import SkeletonGraph
+from neuron_proofreader.merge_proofreading.merge_detection import (
+    MergeDetector,
 )
+from neuron_proofreader.merge_proofreading.search_datasets import (
+    SparseSearchDataset,
+    DenseSearchDataset,
+)
+from neuron_proofreader.models.new_vision_models import NewCNN3D
+from neuron_proofreader.models.vision_models import CNN3D, ViT3D
+from neuron_proofreader.utils import ml_util, util
+
+from neuron_proofreading_evaluation import utils
+from neuron_proofreading_evaluation import visualization as viz
+import neuron_proofreading_evaluation.proofread_merges.merge_evaluation as evaluate
 
 
 def main():
-    # Load merge sites
-    gt_sites = pd.read_csv(gt_sites_path)
-    gt_sites["xyz"] = gt_sites["xyz"].apply(ast.literal_eval)
+    # Load data
+    gt_df = utils.load_sites_df(gt_sites_path)
+    gt_names = gt_df["cell_id"].unique()
+    gt_neurons = load_gt_neurons(gt_names)  # name --> xyz coordinates
+    fragments_datasets = load_datasets(gt_names)  # search_mode --> dataset
 
-    pred_sites = pd.read_csv(pred_sites_path)
-    pred_sites = pred_sites[pred_sites["Prediction"] > threshold]
-    pred_sites["World"] = pred_sites["World"].apply(ast.literal_eval)
-    pred_sites = np.array([xyz for xyz in pred_sites["World"]])
+    # Extract models to evaluate
+    print("\nStep 3: Evaluate Models")
+    model_names = [f for f in util.list_subdirs(models_dir)]
+    print(f"Found {len(model_names)} models in {models_dir}")
 
-    # Run evaluation for each neuron
-    results = list()
-    for gt_id_i, gt_sites_i in tqdm(gt_sites.groupby("cell_id")["xyz"]):
-        # Extract intersection pred sites
-        gt_neuron = read_swc_points(get_gt_path(gt_id_i))
-        gt_kdtree = KDTree(gt_neuron)
-        dd, _ = gt_kdtree.query(pred_sites)
-        pred_sites_i = pred_sites[dd < 5]
+    # Evaluate models
+    for model_name in model_names:
+        # Initializations
+        print(f"\n{'='*(len(model_name) + 4)}")
+        print(f"  {model_name}")
+        print(f"{'='*(len(model_name) + 4)}")
 
-        # Extract true positive pred sites
-        gt_sites_i = np.stack(gt_sites_i.values)
-        gt_sites_kdtree = KDTree(gt_sites_i)
-        dd, _ = gt_sites_kdtree.query(pred_sites_i)
+        # Create model
+        model = create_model(model_name)
+        model_path = get_model_path(model_name)
+        ml_util.load_model(model, model_path)
 
-        # Report performance metrics
-        n_gt = len(gt_sites_i)
-        n_pred = len(pred_sites_i)
-        n_tp = len(pred_sites_i[dd <= 32])
+        # Compute batch size
+        input_shape = (2,) + img_config.patch_shape
+        batch_size = ml_util.find_max_eval_batch_size(model, input_shape)
+        print("Batch Size:", batch_size)
 
-        recall = n_tp / n_gt
-        precision = n_tp / n_pred if n_pred > 0 else 0.0
-        f1 = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0.0
+        # Evaluate model by search type
+        for search_mode, dataset in fragments_datasets.items():
+            # Create output dir
+            dirname = f"{search_mode}-{os.path.splitext(model_name)[0]}"
+            model_output_dir = os.path.join(output_dir, dirname)
+            os.makedirs(model_output_dir, exist_ok=True)
 
-        # Store results
-        results.append({
-            "GroundTruth_ID": gt_id_i,
-            "# GT Sites": n_gt,
-            "# Pred Sites": n_pred,
-            "# TP Sites": n_tp,
-            "Recall": recall,
-            "Precision": precision,
-            "F1": f1
-        })
-        save_sites(gt_id_i, gt_sites_i, pred_sites_i)
+            # Model predictions
+            print("Search Mode:", search_mode)
+            merge_detector = MergeDetector(
+                dataset,
+                model,
+                batch_size=batch_size,
+                threshold=args.threshold,
+            )
+            merge_detector.search_graph()
+            merge_detector.save(model_output_dir, inplace=False)
 
-    # Save results
-    compute_overall_stats(results)
-    path = os.path.join(output_dir, "evaluation_results.csv")
-    df = pd.DataFrame(results)
-    df.to_csv(path, index=False)
-    print(df)
-    #save_skeletons(gt_sites)
+            # Evaluate performance
+            pred_df = load_pred_df(model_output_dir, gt_neurons)
+            evaluate_model(gt_df, pred_df, model_output_dir)
+            stop
 
 
-def compute_overall_stats(results):
-    gt = sum(r["# GT Sites"] for r in results)
-    pred = sum(r["# Pred Sites"] for r in results)
-    tp = sum(r["# TP Sites"] for r in results)
-    weights = [r["# GT Sites"] / gt for r in results]
-    recall = sum(w * r["Recall"] for w, r in zip(weights, results))
-    precision = sum(w * r["Precision"] for w, r in zip(weights, results))
-    f1 = sum(w * r["F1"] for w, r in zip(weights, results))
-    results.append({
-        "GroundTruth_ID": "Overall",
-        "# GT Sites": gt,
-        "# Pred Sites": pred,
-        "# TP Sites": tp,
-        "Recall": recall,
-        "Precision": precision,
-        "F1": f1
-    })
+# --- Evaluation
+def evaluate_model(gt_df, pred_df, output_dir):
+    # Plot model predictions
+    output_path = os.path.join(output_dir, "model_predictions_histogram.png")
+    viz.plot_predictions(pred_df["Prediction"].values, output_path=output_path)
+
+    # Create results preamble
+    preamble = (
+        f"Brain ID: {brain_id}\n"
+        f"Segmentation ID: {segmentation_id}\n"
+        f"{'-' * (len(segmentation_id) + 17)}\n"
+    )
+
+    # Evaluation
+    evaluate.prec_recall_curve(gt_df, pred_df, output_dir)
+    evaluate.prec_recall_per_neuron(gt_df, pred_df, args.threshold, output_dir)
+    evaluate.prec_recall_at_threshold(
+        gt_df, pred_df, args.threshold, output_dir=output_dir, preamble=preamble
+    )
+    evaluate.save_sites(gt_df, pred_df, args.threshold, output_dir, max_dist=32)
 
 
 # --- Load Data ---
-def get_gt_path(gt_id):
-    bucket_name, subpath = util.parse_cloud_path(gt_swcs_path)
-    for path in util.list_gcs_paths(bucket_name, subpath):
-        if gt_id in path:
-            return path
-    raise Exception(f"GT SWC File not Found for {gt_id}")
+def create_model(model_name):
+    # Parse parameters from model name
+    path = os.path.join(models_dir, model_name, "model_config.json")
+    params = util.read_json(path)
 
-
-def read_swc_points(swc_path):
-    reader = swc_util.Reader(verbose=False)
-    swc_dicts = reader(swc_path)
-    return np.array([swc_dict["xyz"] for swc_dict in swc_dicts]).squeeze()
-
-
-def create_swc_names(labels):
-    return set([f"{label}.0" for label in list(labels)])
-
-
-# --- Save Results ---
-def save_sites(gt_id, gt_sites, pred_sites):
-    # Get sites
-    dd, _ = KDTree(gt_sites).query(pred_sites)
-    tp_sites = pred_sites[dd <= 32]
-    fp_sites = pred_sites[dd > 32]
-
-    dd, _ = KDTree(tp_sites).query(gt_sites)
-    fn_sites = gt_sites[dd > 32]
-
-    # Save sites
-    sites_path = os.path.join(output_dir, f"results-{gt_id}.zip")
-    save_points(sites_path, tp_sites, "0.0 1.0 0.0", "true_positive")
-    save_points(sites_path, fp_sites, "1.0 0.0 0.0", "false_positive")
-    save_points(sites_path, fn_sites, "1.0 1.0 1.0", "false_negative")
-
-
-def save_skeletons(gt_sites):
-    # Initializations
-    base_path = f"gs://allen-nd-goog/from_google/{brain_id}/whole_brain/{segmentation_id}/"
-    fragments_path = os.path.join(base_path, "swcs")
-    gt_dataloader = DataLoader(
-        anisotropy=(0.748, 0.748, 1.0),
-        use_anisotropy=True,
-        verbose=False,
-    )
-    fragments_dataloader = DataLoader(
-        anisotropy=(0.748, 0.748, 1.0),
-        use_anisotropy=False,
-        verbose=False,
-    )
-    segmentation = TensorStoreImage(base_path)
-
-    # Save intersecting fragments
-    for gt_id in tqdm(gt_sites["cell_id"].unique(), desc="Save Skeletons"):
-        # Load GT skeleton
-        gt_swc_path = get_gt_path(gt_id)
-        gt_graphs = gt_dataloader.load_groundtruth(gt_swc_path, segmentation)
-        gt_graph = list(gt_graphs.values())[0]
-
-        # Load fragments
-        swc_names = create_swc_names(gt_graph.node_labels())
-        fragment_graphs = fragments_dataloader.load_fragments(
-            fragments_path, swc_names=swc_names
+    # Create model
+    if "newcnn3d" in model_name.lower():
+        input_shape = (2, 128, 128, 128)
+        model = NewCNN3D(
+            input_shape,
+            base_channels=32,
+            depth=params["depth"],
+            max_channels=256,
+            use_resblock=params["use_resblock"],
         )
-        fragment_graphs = data_util.flip_coordinates(fragment_graphs)
+    else:
+        patch_shape = (128, 128, 128)
+        model = CNN3D(
+            patch_shape,
+            n_conv_layers=params["depth"],
+            n_feat_channels=24,
+        )
+    return model
 
-        # Create zip writer
-        zip_path = os.path.join(output_dir, f"results-{gt_id}.zip")
-        with ZipFile(zip_path, "a") as zip_writer:
-            gt_graph.to_zipped_swcs(zip_writer)
-            for key, graph in fragment_graphs.items():
-                graph.to_zipped_swcs(zip_writer)
 
-
-def save_points(zip_path, pts, color, prefix):
-    swc_util.write_points(
-        zip_path,
-        pts,
-        color=color,
-        prefix=prefix,
-        radius=10,
-        write_mode="a"
+def load_datasets(gt_names):
+    # Load graph
+    print("\nStep 2: Load Fragments")
+    swc_paths = unique_fragment_paths(gt_names)
+    graph = SkeletonGraph(
+        anisotropy=(0.748, 0.748, 1.0),
+        node_spacing=5,
+        use_anisotropy=False,
+        verbose=True
     )
+    graph.load(swc_paths)
+
+    # Create datasets
+    datasets = dict()
+    datasets["Sparse"] = SparseSearchDataset(
+        graph,
+        img_config,
+        min_search_size=args.min_search_size,
+    )
+    datasets["Dense"] = DenseSearchDataset(
+        graph,
+        img_config,
+        min_search_size=args.min_search_size,
+        step_size=args.step_size,
+    )
+    return datasets
+
+
+def load_gt_neurons(gt_names):
+    print("\nStep 1: Load Ground Truth")
+    gt_prefix = f"gs://allen-nd-goog/ground_truth_tracings/{brain_id}"
+    gt_neurons = dict()
+    for name in tqdm(gt_names, desc="Read SWCs"):
+        swc_path = f"{gt_prefix}/world/{name}.swc"
+        gt_neurons[name] = utils.load_swc_points(swc_path)
+    return gt_neurons
+
+
+def load_pred_df(output_dir, gt_neurons, max_dist=10):
+    # Reads sites csv
+    csv_path = os.path.join(output_dir, "model_predictions.csv")
+    pred_df = utils.load_sites_df(csv_path)
+
+    # Remove sites far from GT
+    gt_kdtree = KDTree(np.vstack(list(gt_neurons.values())))
+    dd, _ = gt_kdtree.query(np.stack(pred_df["xyz"].values))
+    return pred_df[dd < max_dist]
+
+
+# --- Helpers ---
+def get_img_path():
+    prefixes_path = "/home/jupyter/data/exaspim_image_prefixes.json"
+    prefixes = util.read_json(prefixes_path)
+    #return os.path.join(prefixes[brain_id], "0")
+    return "gs://allen-nd-goog/from_aind/exaSPIM_794495_2026-01-21_14-25-07_training-data/whole-brain-denoised/fused.zarr/0"
+
+
+def get_model_path(model_name):
+    src_dir = os.path.join(models_dir, model_name)
+    paths = util.listdir(src_dir, extension=".pth")
+    assert len(paths) == 1
+    return os.path.join(src_dir, paths[0])
+
+
+def unique_fragment_paths(gt_names):
+    swc_paths = list()
+    visited_fragments = set()
+    n_visited = 0
+    for name in gt_names:
+        prefix = os.path.join(fragments_prefix, name)
+        for swc_path in util.list_paths(prefix):
+            n_visited += 1
+            swc_name = os.path.basename(swc_path)
+            if swc_name not in visited_fragments:
+                swc_paths.append(swc_path)
+                visited_fragments.add(swc_name)
+    return swc_paths
 
 
 if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Process named arguments")
+    parser.add_argument("--brain_id", type=str, required=True)
+    parser.add_argument("--segmentation_id", type=str, required=True)
+    parser.add_argument("--model_version", type=str, required=True)
+    parser.add_argument("--min_search_size", type=float, required=True)
+    parser.add_argument("--step_size", type=float, required=True)
+    parser.add_argument("--threshold", type=float, required=True)
+    args = parser.parse_args()
+
     # Parameters
-    bucket_name = "allen-nd-goog"
-    brain_id = "794495"
-    segmentation_id = "denoised.unet_tri_denoised_2370000"
-    suffix = "20260508_2158"
-    threshold = 0.9
+    brain_id = args.brain_id.strip()
+    segmentation_id = args.segmentation_id.strip()
+
+    img_config = ImageConfig(
+        brightness_clip=500,
+        img_path=get_img_path(),
+        patch_shape=(128, 128, 128),
+        percentiles=(1, 99.9),
+    )
 
     # Paths
-    gt_swcs_path = f"gs://allen-nd-goog/ground_truth_tracings/{brain_id}/world"
-    gt_sites_path = "/home/jupyter/denoised_750k_tri-brain.csv"
-    pred_sites_path = f"/home/jupyter/results/merge_detection/794495/dense-denoised.unet_tri_denoised_2370000/model_predictions.csv"
-    output_dir = f"/home/jupyter/results/evaluate_merge_detection/{brain_id}/dense-{segmentation_id}"
-    util.mkdir(output_dir, delete=True)
+    fragments_prefix = f"gs://allen-nd-goog/anna.grim/intersecting_neuron_fragments/{brain_id}/{segmentation_id}"
+    gt_sites_path = f"/root/capsule/data/{brain_id}-{segmentation_id}.csv"
+    models_dir = f"/root/capsule/scratch/{args.model_version.strip()}}"
+    output_dir = f"/root/capsule/results"
 
     # Run evaluation
     main()
