10a5f4f24a9d01f439cb27afba36bb74de1fe354
Anna Grim <anna.grim@alleninstitute.org>
2026-08-24 05:50:31 +0000

    simplified enforced training recall

--- src/neuron_proofreader/merge_proofreading/merge_datamodules.py
+++ src/neuron_proofreader/merge_proofreading/merge_datamodules.py
@@ -62,7 +62,7 @@ class BrainDataset:
     """
 
     giant_component_cable_length = 30000
-    random_branching_site_probability = 1.0  # 0.70
+    random_branching_site_probability = 0.70
 
     def __init__(
         self,
